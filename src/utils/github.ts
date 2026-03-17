e8fe21bdd972112f172bc8548889ff0d1b74eba3
Alain-RN <alainrakotoarivony926@gmail.com>
2026-03-17 14:32:51 +0300

    _

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -12,7 +12,7 @@ const BASE_URL = "https://api.github.com";
 // Si tu as un token GitHub, mets-le dans ton .env
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
-async function fetchJSON(url: string) {
+async function fetchJSON(url: string, headers: { Authorization: string; } | undefined) {
     const response = await fetch(url, {
         headers: GITHUB_TOKEN
             ? { Authorization: `Bearer ${GITHUB_TOKEN}` }
@@ -26,49 +26,12 @@ async function fetchJSON(url: string) {
     return response.json();
 }
 // Local
-// export async function fetchFormattedProjects(
-//     username: string
-// ): Promise<ProjectRowType[]> {
-//     const repos: GitHubRepo[] = await fetchJSON(
-//         `${BASE_URL}/users/${username}/repos?per_page=100`
-//     );
-
-//     const filteredRepos = repos.filter((repo) => !repo.fork);
-
-//     const projects: ProjectRowType[] = await Promise.all(
-//         filteredRepos.map(async (repo) => {
-//             let technologies: string[] = [];
-
-//             try {
-//                 const languagesData = await fetchJSON(
-//                     `${BASE_URL}/repos/${username}/${repo.name}/languages`
-//                 );
-//                 technologies = Object.keys(languagesData);
-//             } catch {
-//                 technologies = [];
-//             }
-
-//             return {
-//                 year: new Date(repo.created_at).getFullYear(),
-//                 project: repo.name,
-//                 technologies,
-//                 link: repo.html_url,
-//             };
-//         })
-//     );
-
-//     projects.sort((a, b) => b.year - a.year);
-
-//     return projects;
-// }
-
 export async function fetchFormattedProjects(
     username: string
 ): Promise<ProjectRowType[]> {
-    const token = import.meta.env.VITE_GITHUB_TOKEN;
+    const token = import.meta.env.VITE_GITHUB_TOKEN; // Token GitHub injecté via Vercel
     const headers = token ? { Authorization: `token ${token}` } : undefined;
 
-    // Récupérer tous les repos
     const repos: GitHubRepo[] = await fetchJSON(
         `${BASE_URL}/users/${username}/repos?per_page=100`,
         headers
@@ -103,3 +66,4 @@ export async function fetchFormattedProjects(
 
     return projects;
 }
+
